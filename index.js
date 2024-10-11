3de367d48e592f453722623e97fe6c0b0dcf8efc
FAlex357 <alexandrefleurantxbx@gmail.com>
2024-10-11 14:47:36 +0200

    finish

---
+++ index.js
@@ -0,0 +1,18 @@
+// Declare and define three variables using a numerical value
+
+
+const a = 1;
+const b = 2;
+const c = 3;
+// * If the three variables are equal, display "The three variables are the same" in the console
+if (a === b && b === c ){
+  console.log("The three variables are the same");
+}
+// * If only two of the variables are equal, display "Two of the variables are the same" in the console
+else if (a === b || a === c || b === c ){
+console.log("Two of the variables are the same")
+}
+// * If the variables are all different display "all variables are different" in the console
+else {
+  console.log("all variables are different")
+}
